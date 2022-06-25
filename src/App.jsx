75d05bd861fc1bb0fa9ad51b2233f4e6f0d9f566
Galbrush <cathrin.steiner@hotmail.de>
2022-06-25 16:27:28 +0200

    adds recipe switching

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 
+import { useState } from "react"
 import Recipe from "./components/recipe/Recipe"
 import recipes from "./recipes.json"
 
 export default function App() {
     
+
+    const[selectedRecipe, setSelectedRecipe] = useState(recipes.recipes[0])
     
     return (
         <>
-        <Recipe recipe={recipes.recipes[0]}/>                    
+        <select name="Recipes" onChange={(event)=>{setSelectedRecipe(recipes.recipes[event.target.value])}}>
+            {
+                recipes.recipes.map((recipe, index) => {
+                    return (
+                        <option value={index}>{recipe.name}</option>
+                    )
+                })
+            }
+        </select>
+        <Recipe recipe={selectedRecipe}/>
+
 
             
         </>
